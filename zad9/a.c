391f5ded94ca41164d43f119e38206f5d71f45ce
Ivan Tomić <45340642+ivantomi@users.noreply.github.com>
2020-12-17 17:26:39 +0100

    Update a.c

--- zad9/a.c
+++ zad9/a.c
@@ -11,6 +11,7 @@ node Add(int, node);
 position Search(node);
 node Delete(node);
 int Print(node);
+position FindMin(node);
 
 struct treeNode {
 	int element;
@@ -30,10 +31,12 @@ int main()
 	root = Add(1, root);
 	root = Add(8, root);
 	root = Add(3, root);
-	if (find = Search(8, root) != NULL)
+	if (find = Search(17, root) != NULL)
 		printf("Trazeni element je na poziciji %p.\n", find);
-	else printf("nema tog elementa.\n");
-
+	else printf("Nema tog elementa.\n");
+	Print(root);
+	root = Delete(3, root);
+	printf("\n");
 	Print(root);
 }
 
@@ -83,10 +86,46 @@ position Search(int x, node tree)
 	if (tree == NULL)
 		return NULL;
 	else if (x < tree->element)
-		return Search(tree->left);
+		return Search(x, tree->left);
 	else if (x > tree->element)
-		return Search(tree->right);
+		return Search(x, tree->right);
 	return tree;
 }
 
+position FindMin(node tree)
+{
+	if (tree == NULL)
+		return NULL;
+	else if (tree->left == NULL)
+		return tree;
+	else return FindMin(tree->left);
+}
+
+node Delete(int x, node tree)
+{
+	position temp;
+	if (tree == NULL)
+		printf("Taj element ne postoji!\n");
+	else if (x < tree->element)
+		tree->left = Delete(x, tree->left);
+	else if (x > tree->element)
+		tree->right = Delete(x, tree->right);
+	else if (tree->left && tree->right != NULL)
+	{
+		temp = FindMin(tree->right);
+		tree->element = temp->element;
+
+		tree->right = Delete(tree->element, tree->right);
+	}
+	else
+	{
+		temp = tree;
+		if (tree->left == NULL)
+			tree = tree->right;
+		else tree = tree->left;
+		free(temp);
+	}
+
+		return tree;
+}
 
