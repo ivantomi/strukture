1b2d0324e772ebc93254d012f99402311ec8edca
Ivan Tomić <45340642+ivantomi@users.noreply.github.com>
2020-12-29 19:06:42 +0100

    Create Header.c

---
+++ VisualPrint/Header.c
@@ -0,0 +1,166 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include"Header.h"
+#include<stdio.h>
+#include<stdlib.h>
+#define length 12
+
+int Write(position p)
+{
+	int number;
+	for (int i = 1; i < 11; i = i++)
+	{
+		number = i;
+
+		while (p->next != NULL)
+			p = p->next;
+
+		position q = malloc(sizeof(struct node));
+		q->number = number;
+		p->next = q;
+		q->next = NULL;
+	}
+}
+
+int Print(position p)
+{
+	while (p != NULL)
+	{
+		printf("%d ", p->number);
+		p = p->next;
+	}
+}
+
+int VisualPrint(position p)
+{
+	char t = -2;
+	int i = 0;
+	position save = p;
+	int temp;
+	int counter = 1, helper = 0;
+	int overflow = 0;
+
+	//top border
+	while (p != NULL)
+	{
+
+
+		for (i = 0; i < length; i++)
+			printf("%c", t);
+		printf("      ");
+		p = p->next;
+
+	}
+	printf("\n");
+
+	p = save;
+
+	//1st block
+	while (p != NULL)
+	{
+		printf("%c ", t);
+		printf("%p ", p);
+		printf("%c   -> ", t);
+		
+		if (p->next == NULL)
+			printf(" NULL ");
+		p = p->next;
+	}
+	printf("\n");
+
+	p = save;
+
+	//2nd block
+	while (p != NULL)
+	{
+		printf("%c", t);
+		for (i = 0; i < length - 2; i++)
+			printf("-");
+		printf("%c", t);
+		printf("   |  ");
+		p = p->next;
+	}
+
+	p = save;
+	printf("\n");
+
+	//3rd block
+	while (p != NULL)
+	{
+		if (p->number == NULL)
+		{
+			printf("%c   Head   %c   |  ", t, t);
+			p = p->next;
+		}
+		else
+		{
+			temp = p->number;
+			while (temp >= 10)
+			{
+				temp = temp / 10;
+				counter++;
+			}
+			if (counter % 2 == 0)
+				helper = 0;
+			else helper = 1;
+
+			printf("%c", t);
+			for (i = 0; i < (length - 1) / 2 - counter / 2 - helper; i++)
+				printf(" ");
+			printf("%d", p->number);
+
+			for (i = 0; i < (length - 1) / 2 - counter / 2; i++)
+				printf(" ");
+			printf("%c   |  ", t);
+
+			p = p->next;
+		}
+		
+		counter = 1;
+	}
+	printf("\n");
+
+	p = save;
+
+	//4th block
+	while (p != NULL)
+	{
+		printf("%c", t);
+		for (i = 0; i < length - 2; i++)
+			printf("-");
+		printf("%c", t);
+		printf("   |  ");
+		p = p->next;
+	}	
+	printf("\n");
+
+	p = save;
+
+	
+	//5th block
+	while (p != NULL)
+	{
+		printf("%c ", t);
+		if (p->next == NULL)
+			printf("  NULL   ");
+		else
+			printf("%p ", p->next);
+		p = p->next;
+		printf("%c __/  ", t);
+	}
+	printf("\n");
+
+	p = save;
+
+	//bottom border
+	while (p != NULL)
+	{
+		for (i = 0; i < length; i++)
+			printf("%c", t);
+		printf("      ");
+		p = p->next;
+	}
+	printf("\n");
+
+	p = save;
+}
