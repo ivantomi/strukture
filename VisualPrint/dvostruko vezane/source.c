7b92de7215207c27163d59b066b29055d0251e90
Ivan Tomić <45340642+ivantomi@users.noreply.github.com>
2021-01-03 16:39:42 +0100

    Update source.c

--- VisualPrint/dvostruko vezane/source.c
+++ VisualPrint/dvostruko vezane/source.c
@@ -1,1 +1,21 @@
-sadjkvncjksd
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"Header.h"
+
+
+
+int main()
+{
+	struct node head;
+	head.next = NULL;
+	head.prev = NULL;
+	head.number = NULL;
+
+	Write(&head);
+
+	VisualPrintDouble(&head);
+
+
+}
